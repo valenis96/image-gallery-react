cb930b049afbd2d649d4c9190edd5ebb3a2afeca
valenis96 <valentina@maronese.com>
2025-12-08 17:36:17 +0100

    Testing

---
+++ image-gallery/src/componets/app/App.test.tsx
@@ -0,0 +1,31 @@
+import { render, screen } from "@testing-library/react";
+import { beforeEach, describe, expect, test, vi } from "vitest";
+import userEvent from "@testing-library/user-event";
+import App from "./App";
+
+const scrollToMock = vi.fn();
+Object.defineProperty(window, "scrollTo", { value: scrollToMock });
+
+describe("App component", () => {
+  beforeEach(() => {
+    render(<App />);
+    scrollToMock.mockClear();
+  });
+
+  test("Button is render in the app", () => {
+    const button = screen.getByRole("button", { name: /ArrowUpIcon/i });
+    expect(button).toBeInTheDocument();
+  });
+
+  test("on click on the button the page scroll up", async () => {
+    const user = userEvent.setup();
+
+    const button = screen.getByRole("button", { name: /ArrowUpIcon/i });
+    await user.click(button);
+
+    expect(scrollToMock).toHaveBeenCalledWith({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+});
--- image-gallery/vite.config.ts
+++ image-gallery/vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from "path"
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./src/setup-tests.ts",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
